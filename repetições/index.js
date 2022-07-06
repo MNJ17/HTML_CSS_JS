745c5ed269d6492f5e9565a9f3a1ba6de1fb52a5
Monaliza <nascimentodejesusmonaliza522@gmail.com>
2022-07-06 15:42:04 -0300

    exemplos de condicionais

---
+++ repetições/index.js
@@ -0,0 +1,41 @@
+// laço for
+
+/* for (inicializacao; condicao; expressaoFinal) {
+    // codigo
+  } */
+
+ for (let i = 0; i < 5; i++) {
+  console.log(i);
+}
+
+for (let i = 0; i < 5; i++) {
+    if ( i === 3) {
+       break;
+     }
+     console.log(i);
+}
+
+// while
+/* while (condição) {
+    // codigo
+  } */
+
+ let m = 0;
+
+  while (m === 1) {
+    console.log(i);
+    i++;
+  }
+
+// do...while
+
+/* do {
+ //codigo
+} while (condicional) */
+
+let i = 0;
+
+do {
+  console.log(i);
+  i++;
+} while (i === 1);
