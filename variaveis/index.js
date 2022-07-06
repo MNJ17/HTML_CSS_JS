2037f2a1c3bf9c970c8e82a6e17c1a40c3dd7a59
Monaliza <nascimentodejesusmonaliza522@gmail.com>
2022-07-06 17:16:16 -0300

    exemplos de variaveis

---
+++ variaveis/index.js
@@ -0,0 +1,122 @@
+// variaveis
+
+ let mensagem = "olá"
+console.log(mensagem)
+
+ let bomDia = mensagem + " para você"
+console.log(mensagem)
+
+ let mensage = 'olá'
+mensage = 'como você está?'
+console.log(mensage)
+
+ var mensagem1 = "olá";
+let resultadoApos2022 = "positivo";
+let EU_ESTOU = true;
+var _mensagem_ = "ótimo dia"
+var $dinheiro = "cifrão";
+
+ var percentual% = 50;
+let 3anos = true;
+var function = false;
+
+// var
+
+/* var cumprimento = "bom dia!";
+
+    function saudacao() {
+        var mensagem = "como vai?";
+        console.log(mensagem)
+    }
+    saudacao();
+console.log(cumprimento) */
+
+/* var cumprimento = "bom dia!";
+
+    function saudacao() {
+        var mensagem = "como vai?";
+        var mensagem = "como você está?"
+        console.log(mensagem)
+    }
+    saudacao();
+console.log(cumprimento) */
+
+var cumprimento = "bom dia!";
+
+    function saudacao() {
+        var mensagem = "como vai?";
+        mensagem = "como você está?"
+        console.log(mensagem)
+    }
+    saudacao();
+console.log(cumprimento)
+
+console.log(mensagem)
+var mensagem = "como vai?";
+
+var mensagem
+console.log(mensagem)
+var mensagem = "como vai?"
+
+var mensagem = "como vai?"
+var contador = 5
+
+if( contador > 4) {
+    var mensagem = 'saudações!'
+}
+console.log(mensagem)
+
+//LET
+
+ let mensagem = "como vai?"
+let contador = 5
+
+if( contador > 4) {
+    let mensagem = 'saudações!'
+}
+console.log(mensagem)
+
+let mensagem = "como vai?"
+let contador = 5
+
+if( contador > 4) {
+    let mensagem = 'saudações!'
+    mensagem = "bom rever vocês"
+    console.log('mensagem dentro do if', mensagem)
+}
+console.log(mensagem)
+
+ let mensagem = "como vai?"
+let contador = 5
+
+if( contador > 4) {
+    let mensagem = 'saudações!'
+    let mensagem = "bom rever vocês"
+    console.log('mensagem dentro do if', mensagem)
+}
+console.log(mensagem)
+
+ let mensagem = "como vai?"
+if(true) {
+    let mensagem = 'saudações!'
+    console.log('mensagem dentro do bloco: ',mensagem)
+}
+console.log('mensagem fora do bloco if: ',mensagem)
+
+ console.log(mensagem)
+let mensagem = "como vai?";
+
+// CONST
+
+ const mensagem = "como vai?"
+if(true) {
+    const mensagem = 'saudações!'
+    console.log('mensagem dentro do bloco: ',mensagem)
+}
+console.log('mensagem fora do bloco if: ',mensagem)
+
+const mensagem = "como vai?"
+mensagem = 'saudações!'
+
+const mensagem = 'como vai?'
+const mensagem = 'saudações!'
