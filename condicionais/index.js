92c97fa8b3ea4ad90676e855c3739e980214e60d
Monaliza <nascimentodejesusmonaliza522@gmail.com>
2022-07-06 15:55:04 -0300

    exemplos de condicionais

---
+++ condicionais/index.js
@@ -0,0 +1,48 @@
+// if()
+//  let resultado
+//  if (9 > 4) {
+//     resultado = 'bloco de codigo';
+// }
+//  console.log(resultado)
+
+//else( )
+
+if ("azul" === "amarelo") {
+    let resultado = "bloco de codigo do if";
+    console.log(resultado)
+} else {
+    let resultado = "bloco de código else";
+    console.log(resultado)
+}
+
+ //else if( )
+let resultado1 = 'cor'
+
+ if ("azul" === "amarelo") {
+    resultado1 = "bloco de codigo do if";
+} else if ("azul" === "azul") {
+    resultado1 = "bloco de codigo do else if";
+} else {
+     resultado1 = "bloco de codigo else";
+}
+
+console.log(resultado)
+
+// mais um exemplo de else if ( )
+
+let resultado = 'cor'
+
+if ("azul" === "amarelo") {
+    resultado = "bloco de codigo do if";
+} else if ("azul" === "azul") {
+    resultado = "bloco de codigo do else if 1";
+} else if ("azul" === "azul") {
+    resultado = "bloco de codigo do else if 2";
+} else if ("azul" === "vermelho") {
+    resultado = "bloco de codigo do else if 3";
+} else {
+     resultado = "bloco de codigo else";
+}
+console.log(resultado)
+
+
